ccf153dc1b22416d25df527d8376ab8086244ec7
Tan Phu <phantanphu10@gmail.con>
2023-10-11 10:28:28 +0700

    update sql rev03

--- Demo.sql
+++ Demo.sql
@@ -36,27 +36,31 @@ CREATE TABLE `likes` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE `orders` (
+  `id` int NOT NULL AUTO_INCREMENT,
   `user_id` int DEFAULT NULL,
   `food_id` int DEFAULT NULL,
   `amount` int DEFAULT NULL,
   `code_` varchar(200) DEFAULT NULL,
   `arr_sub` varchar(200) DEFAULT NULL,
+  PRIMARY KEY (`id`),
   KEY `user_id` (`user_id`),
   KEY `food_id` (`food_id`),
   CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
   CONSTRAINT `orders_ibfk_2` FOREIGN KEY (`food_id`) REFERENCES `food` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE `rate_res` (
+  `id` int NOT NULL AUTO_INCREMENT,
   `user_id` int NOT NULL,
   `res_id` int NOT NULL,
   `amount` int NOT NULL,
   `date_rate` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
   KEY `user_id` (`user_id`),
   KEY `res_id` (`res_id`),
   CONSTRAINT `rate_res_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
   CONSTRAINT `rate_res_ibfk_2` FOREIGN KEY (`res_id`) REFERENCES `res` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE `res` (
   `id` int NOT NULL AUTO_INCREMENT,
@@ -104,26 +108,34 @@ INSERT INTO `likes` (`user_id`, `res_id`, `date_like`) VALUES
 (4, 4, '2022-07-08 00:00:00'),
 (5, 4, '2022-07-08 00:00:00');
 
-INSERT INTO `orders` (`user_id`, `food_id`, `amount`, `code_`, `arr_sub`) VALUES
-(1, 1, 1, 'a1', 'số 1 nha');
-INSERT INTO `orders` (`user_id`, `food_id`, `amount`, `code_`, `arr_sub`) VALUES
-(1, 1, 2, 'a2', 'số 2 nha');
-INSERT INTO `orders` (`user_id`, `food_id`, `amount`, `code_`, `arr_sub`) VALUES
-(1, 1, 3, 'a3', 'số 3 nha');
-INSERT INTO `orders` (`user_id`, `food_id`, `amount`, `code_`, `arr_sub`) VALUES
-(4, 1, 3, 'a4', 'số 4 nha'),
-(1, 2, 3, 'a5', 'số 5 nha'),
-(3, 1, 3, 'a6', 'số 6 nha'),
-(4, 3, 3, 'a4', 'số 7 nha');
+INSERT INTO `orders` (`id`, `user_id`, `food_id`, `amount`, `code_`, `arr_sub`) VALUES
+(1, 1, 1, 1, 'a1', 'số 1 nha');
+INSERT INTO `orders` (`id`, `user_id`, `food_id`, `amount`, `code_`, `arr_sub`) VALUES
+(2, 1, 1, 2, 'a2', 'số 2 nha');
+INSERT INTO `orders` (`id`, `user_id`, `food_id`, `amount`, `code_`, `arr_sub`) VALUES
+(3, 1, 1, 3, 'a3', 'số 3 nha');
+INSERT INTO `orders` (`id`, `user_id`, `food_id`, `amount`, `code_`, `arr_sub`) VALUES
+(4, 4, 1, 3, 'a4', 'số 4 nha'),
+(5, 1, 2, 3, 'a5', 'số 5 nha'),
+(6, 3, 1, 3, 'a6', 'số 6 nha'),
+(7, 4, 3, 3, 'a4', 'số 7 nha'),
+(8, 1, 3, 12, 'a2', 'số 8 nha');
 
-INSERT INTO `rate_res` (`user_id`, `res_id`, `amount`, `date_rate`) VALUES
-(1, 2, 35, '2022-07-08 00:00:00');
-INSERT INTO `rate_res` (`user_id`, `res_id`, `amount`, `date_rate`) VALUES
-(2, 3, 40, '2022-07-08 00:00:00');
-INSERT INTO `rate_res` (`user_id`, `res_id`, `amount`, `date_rate`) VALUES
-(4, 4, 20, '2022-07-08 00:00:00');
-INSERT INTO `rate_res` (`user_id`, `res_id`, `amount`, `date_rate`) VALUES
-(3, 1, 20, '2022-07-08 00:00:00');
+INSERT INTO `rate_res` (`id`, `user_id`, `res_id`, `amount`, `date_rate`) VALUES
+(1, 1, 2, 35, '2022-07-08 00:00:00');
+INSERT INTO `rate_res` (`id`, `user_id`, `res_id`, `amount`, `date_rate`) VALUES
+(2, 2, 3, 40, '2022-07-08 00:00:00');
+INSERT INTO `rate_res` (`id`, `user_id`, `res_id`, `amount`, `date_rate`) VALUES
+(3, 4, 4, 20, '2022-07-08 00:00:00');
+INSERT INTO `rate_res` (`id`, `user_id`, `res_id`, `amount`, `date_rate`) VALUES
+(4, 3, 1, 20, '2022-07-08 00:00:00'),
+(5, 2, 4, 30, '2023-10-09 17:00:00'),
+(7, 2, 4, 30, '2023-10-10 09:52:00'),
+(8, 2, 4, 30, '2023-10-10 09:52:08'),
+(9, 2, 4, 30, '2023-10-10 09:53:45'),
+(10, 2, 4, 60, '2023-10-10 09:54:17'),
+(11, 2, 4, 60, '2023-10-11 02:55:31'),
+(12, 2, 4, 60, '2023-10-11 02:55:39');
 
 INSERT INTO `res` (`id`, `res_name`, `image`, `descr`) VALUES
 (1, 'nhà hàng 1', 'hình ảnh 1', 'mô tả 1');
